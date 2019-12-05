9272f15f6055aa92378e29c721e76bbca30679fc
Matt Saladna <msaladna@apisnetworks.com>
2019-12-05 02:16:47 -0500

    Switch scanner to Lua. Bypass ACL restrictions on /usr/bin/lua invocation, pass fd to relax permission requirements

---
+++ SOURCES/runAV.lua
@@ -0,0 +1,71 @@
+function fsize(filename)
+   file = io.open(filename,"r")
+   local current = file:seek()
+   local size = file:seek("end")
+   file:seek("set",current)
+   file:close()
+   return size
+end
+
+function main(filename)
+
+   m.log(1,"Inspection for "..filename);
+
+   -- Configure paths
+   local clamdscan  = "/usr/bin/clamdscan"
+   local clamscan  = "/usr/bin/clamscan"
+
+   -- failoverOnClamdFailure: failover to clamscan if clamdscan report an error
+   local failoverOnClamdFailure = true
+
+   -- fail (and block) if clamdscan (and clamscan) fails
+   local failOnError = false
+
+   -- local var
+   local agent = "clamdscan"
+
+   -- Skip empty items because if clamd is not working and you
+   -- use the clamscan agent an empty file can take about 12 secs 
+   -- to be analyzed
+   if fsize(filename) == 0 then
+     m.log(1, "[scanav skipped, file " .. filename .." size is zero]")
+     return nil
+   end
+
+   -- The system command we want to call with fdpass flag to 
+   -- do not incur in a permission issue
+   local cmd = clamdscan .. " --fdpass --stdout --no-summary"
+
+   -- Run the command and get the output
+   local f = io.popen(cmd .. " " .. filename .. " || true")
+   local l = f:read("*a")
+   f:close()
+
+   -- Check the output for the FOUND or ERROR strings which indicate
+   -- an issue we want to block access on
+   local isVuln = string.find(l, "FOUND")
+   local isError = string.find(l, "ERROR")
+  -- If clamdscan fails and you want failover to the traditional clamscan...
+   if isError and failoverOnClamdFailure then
+     -- Try to use the clamscan program
+     m.log(1, "[clamdscan fails (" .. l .. "), failover to clamscan]")
+     agent = "clamscan"
+     cmd = clamscan .. " --stdout --no-summary"
+     f = io.popen(cmd .. " " .. filename .. " || true")
+     l = f:read("*a")
+     f:close()
+     isVuln = string.find(l, "FOUND")
+     isError = string.find(l, "ERROR")
+   end
+
+   if isVuln then
+     m.log(1, "[" .. agent .. " scanner message: " .. l .. "]\n")
+     return "Virus Detected"
+   elseif isError and failOnError then
+     -- is a error (not a virus) a failure event?
+     m.log(1, "[" .. agent .. " scanner message: " .. l .. "]\n")
+     return "Error Detected"
+   else
+     return nil
+   end
+end
--- SPECS/connector.spec
+++ SPECS/connector.spec
@@ -4,13 +4,13 @@
 %global rundir /run
 
 Name:   mod_security-clamav-connector
-Version:  1.0
+Version:  2.0
 Release:  0%{?dist}
 Summary:  mod_security to ClamAV connector
 Group:    System Environment/Daemons
 License:  ASL 2.0
 URL:    https://github.com/apisnetworks/mod_security-clamav-connector
-Source0:  runAV.pl
+Source0:  runAV.lua
 Source1:  mod_security-clamav.conf
 BuildRoot:  %{_tmppath}/%{name}-%{version}-%{release}-root-%(id -nu)
 BuildRequires:  httpd-devel >= 2.4, 
@@ -25,7 +25,7 @@ policy for file uploads in Apache.
 %build
 
 %install
-install -Dm 750 %{SOURCE0} %{buildroot}%{_httpd_modsecdir}/runAV.pl
+install -Dm 750 %{SOURCE0} %{buildroot}%{_httpd_modsecdir}/runAV.lua
 install -Dp -m 0644 %{SOURCE1} %{buildroot}%{_httpd_modsecdir}/activated_rules/clamav.conf
 
 %clean
@@ -34,8 +34,10 @@ rm -rf %{buildroot}
 %files
 %defattr(-,root,root,-)
 %config(noreplace) %{_httpd_modsecdir}/activated_rules/clamav.conf
-%attr(750,root,apache) %{_httpd_modsecdir}/runAV.pl
+%attr(750,root,apache) %{_httpd_modsecdir}/runAV.lua
 
 %changelog
+* Thu Dec 05 2019 Matt Saladna <matt@apisnetworks.com> - 2.0-0
+- Switch scanner to Lua script
 * Sat May 26 2018 Matt Saladna <matt@apisnetworks.com> - 1.0-0
 - Initial release
